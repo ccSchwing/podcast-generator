5d549ab498f8d4389839dd34a07620a44cfd3452
Chuck Schwing <146346009+ccSchwing@users.noreply.github.com>
2024-01-09 12:32:59 +0000

    Trying to fix origin issue in   entrypoint.sh

--- entrypoints.sh
+++ entrypoints.sh
@@ -8,6 +8,6 @@ git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
-git add . ; git commit -m "Update Feed" ; git push --set-upstream origin/01_02b main
+git add -A ; git commit -m "Update Feed" ; git push --set-upstream origin main
 
 echo "++++++++++++++++++++++++++"
