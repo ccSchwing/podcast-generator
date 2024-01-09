b85a47dc48b16b99c0dbfd682a08ddb8e4083a7e
Chuck Schwing <146346009+ccSchwing@users.noreply.github.com>
2024-01-09 12:58:52 +0000

    Set remote branch to 01_2b in entrypoint..sh

--- entrypoints.sh
+++ entrypoints.sh
@@ -9,7 +9,7 @@ git config --global --add safe.directory /github/workspace
 python3 /usr/bin/feed.py
 
 git branch
-git add -A ; git commit -m "Update Feed" ; git push -u --set-upstream origin 01_2b
-
+## git add -A ; git commit -m "Update Feed" ; git push -u --set-upstream origin 01_2b
+git add -A ; git commit -m "Update Feed" ; git push
 
 echo "++++++++++++++++++++++++++"
